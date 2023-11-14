adb3d4116f9cb8445075784d93ed2db032d6f81c
나평근 <ko.itbuddy>
2023-11-15 00:57:10 +0900

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.10"
+    application
+}
+
+group = "com.chuggume"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.6")
+    implementation("org.apache.commons:commons-lang3:3.13.0")
+    implementation("com.microsoft.playwright:playwright:1.28.1")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
