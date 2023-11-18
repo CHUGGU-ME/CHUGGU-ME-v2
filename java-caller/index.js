d2e3c5811d4ae3ab07658b899cc6074127dcceed
안유진 <yoojin.an0@gmail.com>
2023-11-18 14:37:46 +0900

    jar 파일 교체
    - chuggu-me.jar -> CHUGGU-ME-v2-1.0-SNAPSHOT.jar

--- java-caller/index.js
+++ java-caller/index.js
@@ -4,7 +4,7 @@ import { JavaCaller } from "java-caller";
 
 async function main(){
     const java = new JavaCaller({
-        jar: './chuggu-me.jar',
+        jar: './CHUGGU-ME-v2-1.0-SNAPSHOT.jar',
     });
     const { status, stdout, stderr } = await java.run();
     console.log(status)
