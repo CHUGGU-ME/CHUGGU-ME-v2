1c4d881397aad7e47e0111a7656285fe94c1a5c6
Tony <87371627+TonyKim9401@users.noreply.github.com>
2023-11-19 23:03:50 +0900

    Feature/tony (#23)
    * feature: gitignore, Article, FIleIo, PlaywrightUtil, UpdateSubCommand 변경
    
    * refactor: Command와 Service 러프하게 분리
    
    * refactor: 미사용 의존성 제거
    
    * UpdateService, UpdateSubCommand 구조 수정
    
    * refactor: Service&Repository 분리
    
    ---------
    
    Co-authored-by: 나평근 <ko.itbuddy>

--- src/main/kotlin/service/ArticleService.kt
+++ src/main/kotlin/service/ArticleService.kt
@@ -1,13 +1,13 @@
 package service
 
-import common.FileName
+import Repository.ArticleRepository
 import common.OsCommand
-import common.readFromFile
-import domain.News
 
 class ArticleService {
     fun getNewsSelect(no: Int?) {
-        val newsList = readFromFile<MutableList<News>>(FileName.NEWS_LIST.fileName)
+        val articleRepository = ArticleRepository()
+
+        val newsList = articleRepository.getNewsList()
         if (no != null && no <= 10 && no > 0) {
             OsCommand.browser(newsList[no - 1].url)
         } else {
