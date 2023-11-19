a01f8c99bfda14c688ea2709c5d3e91a2d389a54
ktony <k.tony9412@gmail.com>
2023-11-19 22:26:15 +0900

    refactor: Command와 Service 러프하게 분리

--- src/main/kotlin/subcommand/ArticleSubCommand.kt
+++ src/main/kotlin/subcommand/ArticleSubCommand.kt
@@ -1,34 +1,21 @@
 package subcommand
 
-import common.FileName
-import common.OsCommand.Companion.browser
-import common.readFromFile
-import domain.News
 import kotlinx.cli.ExperimentalCli
 import kotlinx.cli.Subcommand
+import service.ArticleService
 
 
 @OptIn(ExperimentalCli::class)
 class ArticleSubCommand:  Subcommand("article", "Article") {
-
-
     override fun execute() {
+        val articleService = ArticleService()
         while(true) {
             println("Enter the number of the article: ")
             val input = readLine()
             if (input == "end") {
                 break
             }
-            getNewsSelect(input?.toIntOrNull())
-        }
-    }
-
-    private fun getNewsSelect(no: Int?) {
-        val newsList = readFromFile<MutableList<News>>(FileName.NEWS_LIST.fileName)
-        if (no != null && no <= 10 && no > 0) {
-            browser(newsList[no-1].url)
-        } else {
-            println("Wrong Input, please try again between 1 to 10")
+            articleService.getNewsSelect(input?.toIntOrNull())
         }
     }
 }
--- src/main/kotlin/subcommand/NewsSubCommand.kt
+++ src/main/kotlin/subcommand/NewsSubCommand.kt
@@ -5,16 +5,12 @@ import common.readFromFile
 import domain.News
 import kotlinx.cli.ExperimentalCli
 import kotlinx.cli.Subcommand
+import service.NewsService
 
 @OptIn(ExperimentalCli::class)
 class NewsSubCommand : Subcommand("news", "News") {
     override fun execute() {
-
-        val resultNews = readFromFile<MutableList<News>>(FileName.NEWS_LIST.fileName)
-        for (news in resultNews) {
-            println("${news.no} : ${news.title}")
-            println("url: ${news.url}")
-            println()
-        }
+        val newsService = NewsService()
+        newsService.getNews()
     }
 }
