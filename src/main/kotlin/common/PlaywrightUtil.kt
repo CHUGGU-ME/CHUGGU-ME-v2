89d7d4fc8a058c875fac812382367143d13ac29d
나평근 <ko.itbuddy>
2023-11-19 14:58:02 +0900

    playwright lateinit 적용

--- src/main/kotlin/common/PlaywrightUtil.kt
+++ src/main/kotlin/common/PlaywrightUtil.kt
@@ -3,16 +3,19 @@ package common
 import com.microsoft.playwright.*
 
 class PlaywrightUtil {
-    fun playWrightUp(): Page {
-        val playwright: Playwright = Playwright.create()
-        val browser: Browser = playwright
-            .chromium()
-            .launch(
-                BrowserType
-                    .LaunchOptions()
-            )
-        val context: BrowserContext = browser.newContext()
-        val page: Page = context.newPage()
-        return page
+    companion object{
+        fun getNewPlayWrightPage(): Page {
+            val playwright: Playwright = Playwright.create()
+            val browser: Browser = playwright
+                .chromium()
+                .launch(
+                    BrowserType
+                        .LaunchOptions()
+                        .setHeadless(false)
+                )
+            val context: BrowserContext = browser.newContext()
+            val page: Page = context.newPage()
+            return page
+        }
     }
 }
