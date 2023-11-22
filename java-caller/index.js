9f86aed6d2e1c7e8895a9892a7988050f5837041
나평근 <ko.itbuddy>
2023-11-23 01:33:55 +0900

    npx 에서 사용 가능하도록 수정

--- java-caller/index.js
+++ java-caller/index.js
@@ -1,17 +1,12 @@
+#!/usr/bin/env node
+const yargs = require('yargs')
+const JavaCaller = require('java-caller');
+const java = new JavaCaller.JavaCaller({
+    jar: './deploy/CHUGGU-ME-v2.jar',
+});
 
-import { JavaCaller } from "java-caller";
-
-
-async function main(){
-    const java = new JavaCaller({
-        jar: './deploy/*.jar',
-        // jar: process.env.JAVA_JAR_PATH,
-    });
-    const { status, stdout, stderr } = await java.run();
-    console.log(status)
+java.run(yargs.argv._).then(({ status, stdout, stderr })=>{
     console.log(stdout)
+}).catch(({  stdout, stderr })=>{
     console.log(stderr)
-}
-
-main();
-
+});
