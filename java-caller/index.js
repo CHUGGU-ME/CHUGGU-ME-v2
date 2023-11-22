6d235bc3cd58ec2b2e9322566c7b4b088a0cb903
안유진 <yoojin.an0@gmail.com>
2023-11-22 22:32:42 +0900

    배포 시 build하는 로직 추가
    - github action이 동작하면서 java-caller/deploy 디렉토리를 생성하고 해당 디렉토리에 jar 파일 복사
    - java-caller에서는 CI시 생성된 deploy하위의 jar파일을 읽어오도록 수정

--- java-caller/index.js
+++ java-caller/index.js
@@ -4,12 +4,13 @@ import { JavaCaller } from "java-caller";
 
 async function main(){
     const java = new JavaCaller({
-        jar: './CHUGGU-ME-v2-1.0-SNAPSHOT.jar',
+        jar: './deploy/*.jar',
+        // jar: process.env.JAVA_JAR_PATH,
     });
     const { status, stdout, stderr } = await java.run();
     console.log(status)
     console.log(stdout)
-    // console.log(stderr)
+    console.log(stderr)
 }
 
 main();
