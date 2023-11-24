9af7f1b206d3f55b2bf40dbb9da3954dbc2239e4
ujoo0408 <ujoo0408@naver.com>
2023-11-25 07:01:18 +0900

    feat: serializable 추가

--- src/main/kotlin/mom/domain/MomInfo.kt
+++ src/main/kotlin/mom/domain/MomInfo.kt
@@ -1,7 +1,9 @@
 package mom.domain
 
 import com.microsoft.playwright.Page
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class MomInfo(
     val matchDate: String,
     val match: String,
@@ -9,18 +11,6 @@ data class MomInfo(
     val goals: String,
     val assist: String,
 ){
-
-    companion object{
-        fun of(page: Page): MomInfo {
-            return MomInfo(
-                matchDate = page.querySelector(".kotm-match-list__time--small").innerText(),
-                match = page.querySelector(".kotm-match-list__fixture").innerText().replace('\n',' '),
-                playerName = page.querySelector(".kotm-match-list__player-name").innerText(),
-                goals = page.querySelector("#mainContent > section > div > div.kotm-results.js-kotm-results.bg-t3 > div.kotm-results__results-column > div.tabbedContent > div.js-fixture-stats-container.kotmModalTab.active > div > div.kotm-results__fixture-stats > div:nth-child(1) > span.kotm-results__fixture-stats-value.kotm-results__fixture-stats-value--bold").innerText(),
-                assist = page.querySelector("#mainContent > section > div > div.kotm-results.js-kotm-results.bg-t3 > div.kotm-results__results-column > div.tabbedContent > div.js-fixture-stats-container.kotmModalTab.active > div > div.kotm-results__fixture-stats > div:nth-child(2) > span.kotm-results__fixture-stats-value.kotm-results__fixture-stats-value--bold").innerText(),
-            )
-        }
-    }
 
     override fun toString(): String{
         val sb: StringBuilder = StringBuilder()
